e6d8a9ce9217ad2e50369c338560a38ba516429c
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-19 16:54:19 -0500

    update-automated-save

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (  
-    "fmt"
+
     "context"
+    "crypto/tls"
+    "crypto/x509"
+    "fmt"
+    "io/ioutil"
     "log"
+
     "github.com/etcd-io/etcd/clientv3"
     "time"
-    "strconv"
+
 )
 
 var (  
@@ -16,12 +21,43 @@ var (
 
 func main() {  
     ctx, _ := context.WithTimeout(context.Background(), requestTimeout)
+    cert, err := tls.LoadX509KeyPair("certs/client.pem", "certs/client-key.pem")
+    caCert, err := ioutil.ReadFile("certs/ca.pem")
+    caCertPool := x509.NewCertPool()
+
+    if err != nil {
+        log.Fatalf("ERR: %v\n",err)
+    }
+
+    caCertPool.AppendCertsFromPEM(caCert)
+
+
+    // Setup HTTPS client
+    tlsConfig := &tls.Config{
+        Certificates: []tls.Certificate{cert},
+        RootCAs:      caCertPool,
+    }
+    //tlsConfig.BuildNameToCertificate()
+
+
     cli, _ := clientv3.New(clientv3.Config{
         DialTimeout: dialTimeout,
-        Endpoints: []string{"127.0.0.1:2379"},
+        Endpoints: []string{"etcd.cwxstat.io:2379"},
+        TLS: tlsConfig,
     })
     defer cli.Close()
     kv := clientv3.NewKV(cli)
 
-    _ = kv
+    GetSingleValueDemo(ctx, kv)
+
+}
+
+func GetSingleValueDemo(ctx context.Context, kv clientv3.KV) {
+    fmt.Println("*** GetSingleValueDemo()")
+
+    // Insert a key value
+    pr, _ := kv.Put(ctx, "slop", "bob")
+    rev := pr.Header.Revision
+    fmt.Println("Revision:", rev)
+
 }
